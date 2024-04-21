446801c6c5cd6199f288fddb0c4366203974dd22
Krijn Grimme <krijngrimme@hotmail.com>
2024-04-21 18:34:09 +0200

    Working code runner with typescript

--- DockerManager/main.go
+++ DockerManager/main.go
@@ -4,36 +4,76 @@ import (
 	"context"
 	"io"
 	"os"
+	"strings"
 
+	"github.com/Windesheim-HBO-ICT/Deeltaken/DockerManager/utility"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
+// This we would host somewhere
+var languageImageMap = map[string]string{
+	"typescript": "lxu-bun",
+	"javascript": "lxu-bun",
+	"python":     "lxu-python",
+	"csharp":     "lxu-csharp",
+}
+
 func main() {
+	// Create a new docker client
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
 	}
 
+	output, err := ExecuteCode(cli, "console.log('Hello, World!')", "typescript")
+	if err != nil {
+		panic(err)
+	}
+
+	println("Output: ", output)
+}
+
+func ExecuteCode(cli *client.Client, input, language string) (string, error) {
 	// Get current working directory
 	cwd, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	// Run a image same as command: `docker run -v "$(pwd)`/program.ts:/input.txt lxu-bun`
+	containerImage := languageImageMap[language]
+
+	// Write the input to a file
+	err = utility.WriteToFile(input, "input.txt")
+	if err != nil {
+		return "", err
+	}
+
+	output, err := RunContainer(cli, containerImage, cwd+"/input.txt")
+	if err != nil {
+		return "", err
+	}
+
+	// Delete the input file
+	_ = os.Remove("./input.txt")
+
+	return output, nil
+}
+
+func RunContainer(cli *client.Client, ImageName, absInFileLoc string) (string, error) {
+	// Run a image same as command: `docker run -v "ABSINFILELOC:/input.txt IMAGENAME`
 	resp, err := cli.ContainerCreate(context.Background(), &container.Config{
-		Image: "lxu-bun",
+		Image: ImageName,
 	}, &container.HostConfig{
-		Binds: []string{cwd + "/program.ts:/input.txt"},
+		Binds: []string{absInFileLoc + ":/input.txt"},
 	}, nil, nil, "")
 
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	if err := cli.ContainerStart(context.Background(), resp.ID, container.StartOptions{}); err != nil {
-		panic(err)
+		return "", err
 	}
 
 	// Wait for the container to finish
@@ -42,22 +82,25 @@ func main() {
 	case err := <-errCh:
 
 		if err != nil {
-			panic(err)
+			return "", err
 		}
 	case <-statusCh:
 	}
 
 	out, err := cli.ContainerLogs(context.Background(), resp.ID, container.LogsOptions{ShowStdout: true})
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	if _, err := io.Copy(os.Stdout, out); err != nil {
-		panic(err)
+	buf := new(strings.Builder)
+	if _, err = io.Copy(buf, out); err != nil {
+		return "", err
 	}
 
 	// Remove the container
 	if err := cli.ContainerRemove(context.Background(), resp.ID, container.RemoveOptions{}); err != nil {
-		panic(err)
+		return "", err
 	}
+
+	return buf.String()[8:], nil
 }
