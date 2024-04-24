56f21323f7dbf94dbb3a3a1fa1f93a8ac822a28d
Krijn Grimme <krijngrimme@hotmail.com>
2024-04-24 22:57:27 +0200

    Fixed some styling

--- CodeBlock/codeBlock.js
+++ CodeBlock/codeBlock.js
@@ -37,6 +37,11 @@ class CodeBlock extends HTMLElement {
   render() {
       this.shadowRoot.innerHTML = `
         <style>
+        select {
+            width: 20%;
+            padding: 0.5rem;
+            min-width: 100px;
+        }
           .flexcol{
               display: flex;
               flex: 1 1 0%;
@@ -51,7 +56,7 @@ class CodeBlock extends HTMLElement {
           .coderunner{
               display: flex;
               height: 100%;
-              width: 91.67%;
+              width: 100%;
               background-color: rgb(226, 232, 240);
               border-radius: 0.5rem;
               box-sizing: border-box;
@@ -89,21 +94,24 @@ class CodeBlock extends HTMLElement {
               width: 100%;
               resize: none;
           }
+          .coderunnerResult{
+              padding: 1.25rem;
+              border-radius: 0.5rem;
+          }
         </style>
         <div class="coderunner">
           <div class="coderunnerHeader">
-            <select id="language">
-            </select>
-                <div class="coderunnerHeaderButtons">
-                  <button id="runButton" class="runButton"><svg width="32px" height="32px" viewBox="0 0 24.00 24.00" fill="lightgreen" xmlns="http://www.w3.org/2000/svg" transform="rotate(90)" stroke="#2afa00"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"></g><g id="SVGRepo_iconCarrier"> <g id="Shape / Triangle"> <path id="Vector" d="M4.37891 15.1999C3.46947 16.775 3.01489 17.5634 3.08281 18.2097C3.14206 18.7734 3.43792 19.2851 3.89648 19.6182C4.42204 20.0001 5.3309 20.0001 7.14853 20.0001H16.8515C18.6691 20.0001 19.5778 20.0001 20.1034 19.6182C20.5619 19.2851 20.8579 18.7734 20.9172 18.2097C20.9851 17.5634 20.5307 16.775 19.6212 15.1999L14.7715 6.79986C13.8621 5.22468 13.4071 4.43722 12.8135 4.17291C12.2957 3.94236 11.704 3.94236 11.1862 4.17291C10.5928 4.43711 10.1381 5.22458 9.22946 6.79845L4.37891 15.1999Z" stroke="#2afa00" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg></button>
-                </div>
+            <select id="language"></select>
+            <div class="coderunnerHeaderButtons">
+              <button id="runButton" class="runButton"><svg width="32px" height="32px" viewBox="0 0 24.00 24.00" fill="lightgreen" xmlns="http://www.w3.org/2000/svg" transform="rotate(90)" stroke="#2afa00"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"></g><g id="SVGRepo_iconCarrier"> <g id="Shape / Triangle"> <path id="Vector" d="M4.37891 15.1999C3.46947 16.775 3.01489 17.5634 3.08281 18.2097C3.14206 18.7734 3.43792 19.2851 3.89648 19.6182C4.42204 20.0001 5.3309 20.0001 7.14853 20.0001H16.8515C18.6691 20.0001 19.5778 20.0001 20.1034 19.6182C20.5619 19.2851 20.8579 18.7734 20.9172 18.2097C20.9851 17.5634 20.5307 16.775 19.6212 15.1999L14.7715 6.79986C13.8621 5.22468 13.4071 4.43722 12.8135 4.17291C12.2957 3.94236 11.704 3.94236 11.1862 4.17291C10.5928 4.43711 10.1381 5.22458 9.22946 6.79845L4.37891 15.1999Z" stroke="#2afa00" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg></button>
             </div>
-            <div class="coderunnerInner">
-              <div id="editor" class="flexcol">
-                <textarea id="codeField" class="codeEditor">${this.getAttribute('initialCode')}</textarea>
-              </div>
-              <div id="output" class="flexcol">
-                <iframe id="resultFrame" width="100%" height="400" title="Result Frame"></iframe>
+          </div>
+          <div class="coderunnerInner">
+            <div id="editor" class="flexcol">
+              <textarea id="codeField" class="codeEditor">${this.getAttribute('initialCode')}</textarea>
+            </div>
+            <div id="output" class="flexcol">
+              <div id="resultFrame" class="coderunnerResult" width="100%" height="50%"></div>
             </div>
           </div>
         </div>
@@ -131,6 +139,9 @@ class CodeBlock extends HTMLElement {
           language: languageDropdown.value,
           code: code
       };
+
+      // Clear the result frame before making a new request
+      resultFrame.innerText = 'Running...';
       
       // Make a POST request to the server
       await fetch('http://localhost:8080/code', {
@@ -143,12 +154,12 @@ class CodeBlock extends HTMLElement {
       .then(response => response.text())
       .then(result => {
           // Display the result received from the server
-          resultFrame.contentDocument.body.innerText = result;
+          resultFrame.innerText = result;
       })
       .catch(error => {
           // Log the error
           console.error('Error:', error);
-          resultFrame.contentDocument.body.innerText = 'An error occurred. Please try again.';
+          resultFrame.innerText = 'An error occurred. Please try again.';
       });
       });
   }
