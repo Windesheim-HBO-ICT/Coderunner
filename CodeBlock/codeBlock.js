b245b31ab4f499daecc379ff6ce9a8c4a8751998
Spatzy <55000624+1spatz1@users.noreply.github.com>
2024-04-24 17:36:48 +0200

    css werkt weer

--- CodeBlock/codeBlock.js
+++ CodeBlock/codeBlock.js
@@ -1,13 +1,13 @@
 class CodeBlock extends HTMLElement {
   constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
+    super();
+    this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
     this.render();
 
-      // Get the language attribute from the custom element
+    // Get the language attribute
     const language = this.getAttribute('language');
     // Get the dropdown element
     const dropdown = this.shadowRoot.querySelector('select');
@@ -20,7 +20,7 @@ class CodeBlock extends HTMLElement {
         <style>
           .flexcol{
               display: flex;
-              flex: 1 1 0%
+              flex: 1 1 0%;
           }
           .coderunnerMain{
               display: flex;
@@ -31,7 +31,7 @@ class CodeBlock extends HTMLElement {
           }
           .coderunner{
               display: flex;
-              height: 100vh;
+              height: 100%;
               width: 91.67%;
               background-color: rgb(226, 232, 240);
               border-radius: 0.5rem;
@@ -58,7 +58,7 @@ class CodeBlock extends HTMLElement {
           .runButton{
               cursor: pointer;
               background: none;
-              border: none
+              border: none;
           }
           .coderunnerInner{
               display: flex;
@@ -71,23 +71,25 @@ class CodeBlock extends HTMLElement {
               resize: none;
           }
         </style>
-        <div class="coderunnerHeader">
-        <select id="language">
-          <option value="javascript">JavaScript</option>
-          <option value="python">Python</option>
-          <option value="java">Java</option>
-        </select>
-            <div class="coderunnerHeaderButtons">
-            <button id="runButton" class="runButton">Run</button>
+        <div class="coderunner">
+          <div class="coderunnerHeader">
+            <select id="language">
+              <option value="javascript">JavaScript</option>
+              <option value="python">Python</option>
+              <option value="java">Java</option>
+            </select>
+                <div class="coderunnerHeaderButtons">
+                  <button id="runButton" class="runButton"><svg width="32px" height="32px" viewBox="0 0 24.00 24.00" fill="lightgreen" xmlns="http://www.w3.org/2000/svg" transform="rotate(90)" stroke="#2afa00"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"></g><g id="SVGRepo_iconCarrier"> <g id="Shape / Triangle"> <path id="Vector" d="M4.37891 15.1999C3.46947 16.775 3.01489 17.5634 3.08281 18.2097C3.14206 18.7734 3.43792 19.2851 3.89648 19.6182C4.42204 20.0001 5.3309 20.0001 7.14853 20.0001H16.8515C18.6691 20.0001 19.5778 20.0001 20.1034 19.6182C20.5619 19.2851 20.8579 18.7734 20.9172 18.2097C20.9851 17.5634 20.5307 16.775 19.6212 15.1999L14.7715 6.79986C13.8621 5.22468 13.4071 4.43722 12.8135 4.17291C12.2957 3.94236 11.704 3.94236 11.1862 4.17291C10.5928 4.43711 10.1381 5.22458 9.22946 6.79845L4.37891 15.1999Z" stroke="#2afa00" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg></button>
+                </div>
             </div>
-        </div>
-        <div class="coderunnerInner">
-            <div id="editor" class="flexcol">
-            <textarea id="codeField" class="codeEditor">${this.getAttribute('initialCode')}</textarea>
+            <div class="coderunnerInner">
+              <div id="editor" class="flexcol">
+                <textarea id="codeField" class="codeEditor">${this.getAttribute('initialCode')}</textarea>
+              </div>
+              <div id="output" class="flexcol">
+                <iframe id="resultFrame" width="100%" height="400" title="Result Frame"></iframe>
             </div>
-            <div id="output" class="flexcol">
-            <iframe id="resultFrame" width="100%" height="400" title="Result Frame"></iframe>
-            </div>
+          </div>
         </div>
       `;
       this.initializeCodeRunner(this.getAttribute('language'), this.getAttribute('initialCode'));
@@ -108,13 +110,13 @@ class CodeBlock extends HTMLElement {
     
       const code = codeInput.value; // Get code from the textarea
       
-          // Prepare data to send to the server
+      // Prepare data to send to the server
       const requestData = {
           language: languageDropdown.value,
           code: code
       };
       
-          // Make a POST request to the server
+      // Make a POST request to the server
       await fetch('http://localhost:8080/code', {
           method: 'POST',
           headers: {
@@ -124,7 +126,7 @@ class CodeBlock extends HTMLElement {
       })
       .then(response => response.text())
       .then(result => {
-          // Display the result received from the server in the iframe
+          // Display the result received from the server
           resultFrame.contentDocument.body.innerText = result;
       })
       .catch(error => {
