cdb0bbdca21af8cbce95b80fb5be1f1348e8a9fd
Krijn Grimme <krijngrimme@hotmail.com>
2024-04-25 11:56:03 +0200

    Fixed lxu not getting newlines

--- LXU/python/script.sh
+++ LXU/python/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Get the input code from the first argument
-echo $1 > /input.txt
-
 # Copy the input code to the ts program file
 cp /input.txt /source/program.py
 
 # Run the program with bun
 python3 /source/program.py
+
+# Empty the input file
+echo "" > /input.txt
